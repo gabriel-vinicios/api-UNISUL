dc193591c43c030053aae68dbb0b9fcafc4bc1a2
gabriel-vinicios <crisishd2332@gmail.com>
2023-05-03 20:09:33 -0300

    styling

--- consumo_api/src/App.js
+++ consumo_api/src/App.js
@@ -14,39 +14,39 @@ function App() {
     porque usamos async. */
     async function getFilmes(search = 'start%20trek') {
       const encodedURI = search.replace(/ /g, '%20')
-    const response = await api.get(encodedURI);
-    /* Colocamos o console log para poder inspecionar
-    os elementos para debug, mas não é utilizado na
-    aplicação*/
-    //console.log(response.data);
-    // ... Continua .... 
-    // 6. jovar os valores recebidos para filmes
-    
-    setFilmes(response.data);
-  }
+      const response = await api.get(encodedURI);
+      /* Colocamos o console log para poder inspecionar
+      os elementos para debug, mas não é utilizado na
+      aplicação*/
+      //console.log(response.data);
+      // ... Continua .... 
+      // 6. jovar os valores recebidos para filmes
 
-  getFilmes(pesquisa)
+      setFilmes(response.data);
+    }
+
+    getFilmes(pesquisa)
   }, [pesquisa])
   // 2. Criamos nosso render
   return (
     /* 7. Vamos buscar os dados de this.state e jogar para a posição filmes*/
-    
-      <div>
-        <h1>Filmes</h1>
-        <input type="text" onChange={(e) => setPesquisa(e.target.value)}/>
-        {console.log(filmes)}
-        {/* 8. Como temos uma array, então vamos usar um maps para percorrê-la. Vejaj que temos que ver
+
+    <div>
+      <h1>Filmes</h1>
+      <input type="text" onChange={(e) => setPesquisa(e.target.value)} />
+      {console.log(filmes)}
+      {/* 8. Como temos uma array, então vamos usar um maps para percorrê-la. Vejaj que temos que ver
 no resultado da API os atributos que vamos mostrar, como name e url.*/}
-        {filmes?.map(filme => (
-          <li key={filme.show.id}>
-            <h2>
-              {filme.show.name}
-            </h2>
-            <p><a href={filme.show.url} target="_blank">
-              {filme.show.url}</a></p>
-          </li>
-        ))}
-      </div>
-    );
+      {filmes?.map(filme => (
+        <li key={filme.show.id}>
+          <h2>
+            {filme.show.name}
+          </h2>
+          <p><a href={filme.show.url} target="_blank">
+            {filme.show.url}</a></p>
+        </li>
+      ))}
+    </div>
+  );
 }
 export default App;
